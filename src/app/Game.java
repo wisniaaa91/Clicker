ea24c8366208c815eb4a7c5c017459b33f7a642a
Patryk <pwisniewski1991@gmail.com>
2019-08-23 21:01:06 +0200

    Add save, load and reset

--- src/app/Game.java
+++ src/app/Game.java
@@ -3,6 +3,8 @@ package app;
 import java.awt.*;
 import java.awt.event.*;
 
+import java.io.*;
+
 import javax.swing.*;
 
 public class Game implements ActionListener{
@@ -10,7 +12,9 @@ public class Game implements ActionListener{
     JFrame gameFrame;
     int counter = 0;
     JButton clickButton;
+    JButton resetButton;
     JLabel gameScore;
+    int score;
 
     public void init(){
         //Create and set up the window.
@@ -18,23 +22,23 @@ public class Game implements ActionListener{
         gameFrame.setSize(500, 700);
         gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //Create a panel for object.
-        JPanel gamePanel = new JPanel();
-        
-        //Create a button.
+        //Create a buttons.
         clickButton = new JButton("CLICK HERE TO GET SCORE");
+        resetButton = new JButton("Reset Game");
+        clickButton.setSize(300, 30);
 
         //Add actionListener to button.
         clickButton.addActionListener(this);
-
-        //Add button to panel.
-        gamePanel.add(clickButton);
+        resetButton.addActionListener(new ResetGameActionListener());
 
         //Add top label
         JLabel topLabel = new JLabel("Score: ");
         JPanel topPanel = new JPanel();
         topPanel.add(topLabel);
         gameFrame.getContentPane().add(BorderLayout.NORTH, topPanel);
+
+        //Load score from last sesion.
+        loadGame();
 
         //Add label with show score.
         gameScore = new JLabel(String.valueOf(counter));
@@ -43,15 +47,53 @@ public class Game implements ActionListener{
         gameFrame.getContentPane().add(BorderLayout.CENTER, gameScorePanel);
 
         //Place button on the buttom of game window.
-        gameFrame.getContentPane().add(BorderLayout.SOUTH, gamePanel);
+        gameFrame.getContentPane().add(BorderLayout.SOUTH, clickButton);
+        gameFrame.getContentPane().add(BorderLayout.WEST, resetButton);
 
         //Display the window.
         gameFrame.setVisible(true);
+    }
+
+    //Save score method to the file.
+    public void saveGame(){
+        try{
+            File saveFile = new File("save.bin");
+            FileWriter fileWriter = new FileWriter(saveFile);
+            fileWriter.write(score);
+            fileWriter.close();
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    //Load score method from the file.
+    public void loadGame(){
+        try{
+            File loadFile = new File("save.bin");
+            FileReader fileReader = new FileReader(loadFile);
+            counter = fileReader.read();
+
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public void resetGame(){
+        counter = 0;
+        gameScore.setText(String.valueOf(counter));
+        saveGame();
+    }
+    public class ResetGameActionListener implements ActionListener{
+        public void actionPerformed(ActionEvent ev){
+            resetGame();
+        }
     }
 
     public void actionPerformed(ActionEvent arg0) {
         counter++;
         clickButton.setText("click");
         gameScore.setText(String.valueOf(counter));
+        score = counter;
+        saveGame();
     }
 }
